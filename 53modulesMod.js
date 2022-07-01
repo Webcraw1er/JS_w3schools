c4f7b8e30990e1fa1ede92974f847aec59a80a66
Justin <aube9999@naver.com>
2022-07-01 17:32:48 +0900

    202207011730

---
+++ 53modulesMod.js
@@ -0,0 +1,12 @@
+class cars {
+    constructor(name, year){
+        this.name= name;
+        this.year= year;
+    }
+
+}
+
+let car1= new cars("Volvo", 2022);
+let car2= new cars("Bentz", 2020);
+
+export {car1, car2};
